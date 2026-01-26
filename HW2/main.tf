5f7b8298f28c3d2ea950ba6f1be1cd4ba2608496
Arjun Avadhani <arjunavadhani22@gmail.com>
2026-01-26 11:24:38 -0500

    Revert "feat: Add user_data script for automated Docker deployment"
    This reverts commit 141b605b43849f3a8cf3a7cd0beb0766030fdc8f.

--- HW2/main.tf
+++ HW2/main.tf
@@ -10,15 +10,9 @@ variable "ssh_key_name" {
   description = "Name of your existing AWS key pair"
 }
 
-# Your GitHub repository URL
-variable "git_repo_url" {
-  type        = string
-  description = "URL of your GitHub repository to clone"
-}
-
 # The provider of your cloud service, in this case it is AWS. 
 provider "aws" {
-  region = "us-west-2" # Which region you are working on
+  region     = "us-west-2" # Which region you are working on
 }
 
 # Your ec2 instance
@@ -26,47 +20,16 @@ resource "aws_instance" "demo-instance" {
   ami                    = data.aws_ami.al2023.id
   instance_type          = "t2.micro"
   iam_instance_profile   = "LabInstanceProfile"
-  vpc_security_group_ids = [aws_security_group.ssh.id, aws_security_group.web.id]
+  vpc_security_group_ids = [aws_security_group.ssh.id]
   key_name               = var.ssh_key_name
 
-  # This script runs automatically when the instance first boots
-  user_data = <<-EOF
-              #!/bin/bash
-              # Log all output for debugging
-              exec > /var/log/user-data.log 2>&1
-              set -x
-
-              # Update the system
-              yum update -y
-
-              # Install Git
-              yum install -y git
-
-              # Install Docker
-              yum install -y docker
-              systemctl start docker
-              systemctl enable docker
-
-              # Add ec2-user to docker group
-              usermod -aG docker ec2-user
-
-              # Clone your repository
-              cd /home/ec2-user
-              git clone ${var.git_repo_url} app
-              chown -R ec2-user:ec2-user app
-
-              # Navigate to the docker-gs-ping directory and build/run the container
-              cd app/HW2/docker-gs-ping
-              docker build -t docker-gs-ping .
-              docker run -d -p 8080:8080 docker-gs-ping
-              EOF
-
   tags = {
-    Name = "terraform-docker-instance"
+    Name = "terraform-created-instance-:)"
   }
 }
 
-# Security group for SSH access from your IP
+# Your security that grants ssh access from 
+# your ip address to your ec2 instance
 resource "aws_security_group" "ssh" {
   name        = "allow_ssh_from_me"
   description = "SSH from a single IP"
@@ -76,25 +39,6 @@ resource "aws_security_group" "ssh" {
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = [var.ssh_cidr]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-# Security group for HTTP traffic (so you can access your app)
-resource "aws_security_group" "web" {
-  name        = "allow_web_traffic"
-  description = "Allow inbound HTTP traffic for the app"
-  ingress {
-    description = "HTTP on port 8080"
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Allows access from anywhere
   }
   egress {
     from_port   = 0
@@ -116,12 +60,4 @@ data "aws_ami" "al2023" {
 
 output "ec2_public_dns" {
   value = aws_instance.demo-instance.public_dns
-}
-
-output "ec2_public_ip" {
-  value = aws_instance.demo-instance.public_ip
-}
-
-output "app_url" {
-  value = "http://${aws_instance.demo-instance.public_ip}:8080/albums"
 }
